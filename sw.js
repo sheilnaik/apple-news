795880673392ab468a3da34178a66ca647c86a09
Sheil Naik <sheilnaik@users.noreply.github.com>
2026-08-22 23:04:28 -0400

    Keep PWA masthead below iOS status glass

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "apple-news-shell-v2";
+const CACHE_NAME = "apple-news-shell-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css",
+  "./styles.css?v=3",
   "./app.js",
   "./manifest.webmanifest",
   "./icons/app-icon.svg",
