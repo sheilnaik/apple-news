13eb5a1a2c6f563fa364a51d878ab887384e3dc1
Sheil Naik <sheilnaik@users.noreply.github.com>
2026-09-03 21:57:50 -0400

    Refine publication grid styling

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "apple-news-shell-v9";
+const CACHE_NAME = "apple-news-shell-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=9",
+  "./styles.css?v=10",
   "./app.js",
   "./manifest.webmanifest",
   "./logos/404-media.png",
