0bc5c706036154b5b38bf2f3b6702768f98cabd2
sheilnaik <sheilnaik@users.noreply.github.com>
2026-09-04 01:53:30 +0000

    Deploy preview for PR 11 🛫

---
+++ pr-preview/pr-11/sw.js
@@ -0,0 +1,77 @@
+const CACHE_NAME = "apple-news-shell-v9";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css?v=9",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./logos/404-media.png",
+  "./logos/bloomberg-businessweek.png",
+  "./logos/bloomberg.png",
+  "./logos/central-jersey-news.png",
+  "./logos/christian-science-monitor.png",
+  "./logos/economist.png",
+  "./logos/mit-technology-review.png",
+  "./logos/new-scientist.png",
+  "./logos/reuters.png",
+  "./logos/semafor.png",
+  "./logos/sportico.png",
+  "./logos/techcrunch.png",
+  "./logos/the-athletic.png",
+  "./logos/the-verge.png",
+  "./logos/wall-street-journal.png",
+  "./logos/wired.png",
+  "./icons/app-icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(
+      (cached) =>
+        cached ||
+        fetch(event.request).then((response) => {
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+    )
+  );
+});
