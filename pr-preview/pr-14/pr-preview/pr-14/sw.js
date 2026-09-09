5e371af7817b64ed80c758a8345c003666ef0622
sheilnaik <sheilnaik@users.noreply.github.com>
2026-09-09 17:31:27 +0000

    Deploy preview for PR 14 🛫

--- pr-preview/pr-14/pr-preview/pr-14/sw.js
+++ pr-preview/pr-14/pr-preview/pr-14/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "apple-news-shell-v14";
+const CACHE_NAME = "apple-news-shell-v13";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=12",
+  "./styles.css?v=11",
   "./app.js",
   "./manifest.webmanifest",
   "./logos/404-media.png",
